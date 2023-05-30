b5615d6ba97d36f7f1466e3193260d613aafb34f
linhvq <linhvq.ho@mcredit.com.vn>
2023-05-30 10:34:55 +0700

    update

--- app/main.tf
+++ app/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-1"
   # ... other provider settings ...
 }
-terraform {
-  backend "s3" {}
-}
+# terraform {
+#   backend "s3" {}
+# }
 module "app" {
   source = "../../../app"
   instance_type  = var.instance_type
